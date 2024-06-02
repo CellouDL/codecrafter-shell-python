82b2856bcd4d8a1519cf4a3dd800289664e15551
Cellou <72306494+CellouDL@users.noreply.github.com>
2024-06-01 21:51:59 -0400

    echo + builtin

--- codecrafters-shell-python/app/main.py
+++ codecrafters-shell-python/app/main.py
@@ -2,13 +2,24 @@ import sys
 
 
 def main():
+
+    commands = ["echo", "exit", "type"]
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input()
         if command=="exit 0":
             break
-        sys.stdout.write(f"{command}: command not found\n")
+        elif command.startswith("echo "):
+            print(command[5:])
+        elif command.startswith("type "):
+            if command[5:] in commands:
+                sys.stdout.write(f"{command[5:]} is a shell builtin\n")
+            else:
+                sys.stdout.write(f"{command[5:]} not found\n")
+
+        else:
+            sys.stdout.write(f"{command}: command not found\n")
         sys.stdout.flush()
 
 

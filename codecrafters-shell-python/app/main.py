c4b6065ec481bf4bca073119b277499eb17f3bff
Cellou <72306494+CellouDL@users.noreply.github.com>
2024-06-01 20:12:16 -0400

    handling missing commands

--- codecrafters-shell-python/app/main.py
+++ codecrafters-shell-python/app/main.py
@@ -2,16 +2,13 @@ import sys
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    # sys.stdout.write("$ ")
-    # sys.stdout.flush()
+    sys.stdout.write("$ ")
+    sys.stdout.flush()
 
     # Wait for user input
-    input()
-
+    command = input()
+    sys.stdout.write(f"{command}: command not found\n")
 
 if __name__ == "__main__":
     main()
+

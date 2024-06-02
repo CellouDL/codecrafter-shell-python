4b11e563df102220aab8f25d55f05e8313630fe6
Cellou <72306494+CellouDL@users.noreply.github.com>
2024-06-02 09:13:43 -0400

    Add PATH and functions

--- codecrafters-shell-python/app/main.py
+++ codecrafters-shell-python/app/main.py
@@ -1,25 +1,40 @@
 import sys
+import os
 
+def echo(args):
+    print(" ".join(args))
+
+def exit0():
+    quit()
 
 def main():
 
     commands = ["echo", "exit", "type"]
+    paths = os.environ.get("PATH").split(":")
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input()
+        cmd, *args = command.split(" ")
         if command=="exit 0":
-            break
-        elif command.startswith("echo "):
-            print(command[5:])
-        elif command.startswith("type "):
-            if command[5:] in commands:
-                sys.stdout.write(f"{command[5:]} is a shell builtin\n")
+            exit0()
+        elif cmd=="echo":
+            echo(args)
+        elif cmd=="type":
+            cmd = args[0]
+            path = None
+            for i in paths:
+                if os.path.isfile(f"{i}/{cmd}"):
+                    path = f"{i}/{cmd}"
+            if args[0] in commands:
+                sys.stdout.write(f"{cmd} is a shell builtin\n")
+            elif path:
+                sys.stdout.write(f"{cmd} is {path}\n")
             else:
-                sys.stdout.write(f"{command[5:]} not found\n")
+                sys.stdout.write(f"{cmd} not found\n")
 
         else:
-            sys.stdout.write(f"{command}: command not found\n")
+            sys.stdout.write(f"{cmd}: command not found\n")
         sys.stdout.flush()
 
 
